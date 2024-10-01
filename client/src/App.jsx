da955759806fe7a3dc161340c6b6922edfbe4e46
lehoan411 <notlehoan411@gmail.com>
2024-10-01 20:14:27 +0700

    manage products

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,21 +7,31 @@ import Cart from './pages/Cart/Cart'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import MyOrders from './pages/MyOrders/MyOrders'
-
+import CreateProductPopup from './pages/ManageProducts/CreateProductPopup'
+import ManageProducts from './pages/ManageProducts/ManageProducts'
+import UpdateProductPopup from './pages/ManageProducts/UpdateProductPopup'
+import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
-
-  const [showLogin,setShowLogin] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [createProduct, setCreateProduct] = useState(false);
+  const [updateProduct, setUpdateProduct] = useState(false);
 
   return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : null}
+      {createProduct ? <CreateProductPopup setCreateProduct={setCreateProduct} /> : null}
+      {updateProduct ? <UpdateProductPopup setUpdateProduct={setUpdateProduct} /> : null}
       <div className='app'>
-        <Navbar setShowLogin={setShowLogin}/>
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/order' element={<PlaceOrder/>}/>
-          <Route path='/myorder' element={<MyOrders/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+          <Route path='/myorder' element={<MyOrders />} />
+          <Route 
+            path='/manage' 
+            element={<ManageProducts setCreateProduct={setCreateProduct} setUpdateProduct={setUpdateProduct} />}  
+          />
         </Routes>
       </div>
       <Footer />
@@ -29,4 +39,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
