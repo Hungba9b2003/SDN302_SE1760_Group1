6b32dca844f36bfa4aa3a694ce223929fd5f44b0
lehoan411 <notlehoan411@gmail.com>
2024-10-07 23:38:29 +0700

    update manage dish

--- client/src/pages/ManageProducts/CreateProductPopup.jsx
+++ client/src/pages/ManageProducts/CreateProductPopup.jsx
@@ -4,31 +4,43 @@ import { assets } from '../../assets/assets'
 import PropTypes from 'prop-types';
 
 
-const CreateProductPopup = ({setCreateProduct}) => {
+const CreateProductPopup = ({ setCreateProduct }) => {
 
-    const [currState,setCurrState] = useState("create");
+  const [currState, setCurrState] = useState("create");
 
   return (
     <div className='create-popup'>
-        <div className="create-popup-container">
-            <div className="create-popup-title">
-                <h2>{currState}</h2> <img onClick={()=>setCreateProduct(false)} src={assets.cross_icon} alt="" />
-            </div>
-            <div className="create-popup-inputs">
-                <input type="text" placeholder='Product name' />
-                <input type="text" placeholder='Price' />
-                <input type="number" placeholder='quantity' />
-
-            </div>
-            <button>Create</button>
+      <div className="create-popup-container">
+        <div className="create-popup-title">
+          <h2>{currState}</h2> <img onClick={() => setCreateProduct(false)} src={assets.cross_icon} alt="" />
         </div>
+        <div className="create-popup-inputs">
+          <div  >Image</div>
+          <input type="text" placeholder='Image url' />
+          <div>Name</div>
+          <input type="text" placeholder='Product name' />
+          <div>Price</div>
+          <input type="text" placeholder='Price' />
+          <div>Description</div>
+          <input type="text" placeholder='description' />
+          <div>Category</div>
+          <select className="form-select" aria-label="Default select example">
+            <option defaultValue={"sdaw"}>noddle</option>
+            <option value="fsa">dessert </option>
+            <option value="daw">snack</option>
+            <option value="ewad">drink</option>
+          </select>
+
+        </div>
+        <button>Create</button>
+      </div>
     </div>
   )
-  
+
 };
 CreateProductPopup.propTypes = {
-    setCreateProduct: PropTypes.func.isRequired,
-  };
+  setCreateProduct: PropTypes.func.isRequired,
+};
 
 
 export default CreateProductPopup
--- client/src/pages/ManageProducts/ManageProducts.jsx
+++ client/src/pages/ManageProducts/ManageProducts.jsx
@@ -8,45 +8,71 @@ const ManageProducts = ({ setCreateProduct, setUpdateProduct }) => {
     return (
         <div className='manage'>
             <div className='create'>
-                        <button onClick={() => setCreateProduct(true)}>Create Product</button>
-                    </div>
+                <button onClick={() => setCreateProduct(true)}>Create Product</button>
+            </div>
             <div className='container'>
-            <div className='row'>
-                <div className='col-md-2'>
-                    <select className="form-select" aria-label="Default select example">
-                        <option defaultValue={"sdaw"}>Order by</option>
-                        <option value="fsa">One</option>
-                        <option value="daw">Two</option>
-                        <option value="ewad">Three</option>
-                    </select>
-                </div>
-                <div className='col-md-8 search'>
-                    <input type="text" placeholder='Search product' />
-                </div>
-                <div className='col-md-2 search-button'>
-                    <button >Search</button>
+                <div className='row'>
+                    <div className='col-md-2'>
+                        <select className="form-select" aria-label="Default select example">
+                            <option defaultValue={"sdaw"}>Order by</option>
+                            <option value="fsa">Alphabet</option>
+                            <option value="daw">Price</option>
+                        </select>
+                    </div>
+                    <div className='col-md-8 search'>
+                        <input type="text" placeholder='Search product' />
+                    </div>
+                    <div className='col-md-2 search-button'>
+                        <button >Search</button>
+                    </div>
                 </div>
             </div>
-            </div>
-            
+
 
             <div className="manage-items">
-                <div className="manage-items-title">
-                    <p>Items</p> <p>Title</p> <p>Price</p> <p>Quantity</p> <p>Edit</p> <p>Remove</p>
-                </div>
-                <br />
-                <hr />
-
-
-                <div>
-                    <div className="manage-items-title manage-items-item">
-                        <img src="" alt="" />
-                        <p>sd</p>
-                        <p>daw</p>
-                        <div>1</div>
-                        <p className='manage-items-remove-icon' onClick={() => setUpdateProduct(true)}><i className="fa-solid fa-pen"></i></p>
-                        <p className='manage-items-remove-icon' onClick={() => removeFromManage(item.food_id)}><i className="fa-solid fa-trash"></i></p>
+                <div className="manage-items-title row">
+                    <div className='col-md-2'>
+                        <p>Items</p>
                     </div>
+                    <div className='col-md-3'>
+                        <p>Dish</p>
+                    </div>
+                    <div className='col-md-1'>
+                        <p>Price</p>
+                    </div>
+                    <div className='col-md-4'>
+                        <p>Description</p>
+                    </div>
+                    <div className='col-md-1'>
+                        <p>Edit</p>
+                    </div>
+                    <div className='col-md-1'>
+                        <p>Remove</p>
+                    </div>
+
+                </div>
+                
+                <hr />
+                    <div className="row manage-items-title manage-items-item ">
+                        <div className='col-md-2'>
+                            <img src="https://media-api.advertisingvietnam.com/oapi/v1/media?uuid=3aab6b15-7347-48fd-b189-a56870880156&resolution=1000x&keepOriginal=true" alt="" />
+                        </div>
+                        <div className='col-md-3'>
+                            <p>sd</p>
+                        </div>
+                        <div className='col-md-1'>
+                            <p>12$</p>
+                        </div>
+                        <div className='col-md-4'>
+                            <p>Descriptiondddddddddddddddddddddddddbhfdiubiadeshuedawhu</p>
+                        </div>
+                        <div className='col-md-1'>
+                            <p className='manage-items-remove-icon' onClick={() => setUpdateProduct(true)}><i className="fa-solid fa-pen"></i></p>
+                        </div>
+                        <div className='col-md-1'>
+                            <p className='manage-items-remove-icon' onClick={() => removeFromManage(item.food_id)}><i className="fa-solid fa-trash"></i></p>
+                        </div>
+                        
                     <hr />
                 </div>
 
